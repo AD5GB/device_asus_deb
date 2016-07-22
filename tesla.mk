1878a63b5c06c50fcc840259830a70495a3c474d
Randy DuCharme <radio.ad5gb@gmail.com>
2016-07-21 20:15:31 -0500

    Slim repo for deb
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

---
+++ tesla.mk
@@ -0,0 +1,29 @@
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+##TARGET_BOOTANIMATION_HALF_RES := true
+
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_tablet_wifionly.mk)
+
+# Inherit telephony stuff
+# $(call inherit-product, vendor/tesla/config/telephony.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/full_deb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := tesla_deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razorg \
+    BUILD_FINGERPRINT=google/razorg/deb:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="razorg-user 6.0.1 MMB29Q 2480792 release-keys"
